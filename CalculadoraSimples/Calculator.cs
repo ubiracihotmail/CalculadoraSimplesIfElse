2dcfb7de153f44c7e9898523f5a2b465f502e908
UbiraciOliveira <ubira.domingos@gmail.com>
2025-05-10 19:36:02 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraSimples/Calculator.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CalculadoraSimplesIfElse
+//{
+//    internal class Calculator
+//    {
+//        public int Calculate(string operation, int value1, int value2)
+//        {
+//            if (operation == "+")
+//            {
+//                return value1 + value2;
+//            }
+//            else if (operation == "-")
+//            {
+//                return value1 - value2;
+//            }
+//            else if (operation == "*")
+//            {
+//                return value1 * value2;
+//            }
+//            else if (operation == "/")
+//            {
+//                return value1 / value2;
+//            }
+//            return 0;
+//        }
+
+//    }
+//}
---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,44 @@
+﻿using CalculadoraSimplesIfElse;
+
+Console.Write("Digite um número ");
+var calc1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+Console.Write("Digite o outro número ");
+var calc2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+Console.Write("Digite o operador matemático ");
+var operate = Console.ReadLine()!;
+Console.WriteLine();
+
+Calculator calculator = new Calculator();
+Console.WriteLine($"Resultado: {calculator.Calculate (operate, calc1, calc2)}"); 
+
+namespace CalculadoraSimplesIfElse
+{
+    internal class Calculator
+    {
+        public int Calculate(string operation, int value1, int value2)
+        {
+            if (operation == "+")
+            {
+                return value1 + value2;
+            }
+            else if (operation == "-")
+            {
+                return value1 - value2;
+            }
+            else if (operation == "*")
+            {
+                return value1 * value2;
+            }
+            else if (operation == "/")
+            {
+                return value1 / value2;
+            }
+            return 0;
+        }
+
+    }
+}
